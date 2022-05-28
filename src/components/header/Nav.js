893e6a32d3e3c03b2526e424d00e7ee57f89b5ab
emilioMarchi <em.98.nmynm@gmail.com>
2022-05-28 14:37:48 -0300

    commit

--- src/components/header/Nav.js
+++ src/components/header/Nav.js
@@ -1,27 +1,30 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import {BrowserRouter as Router ,Link } from 'react-router-dom';
 import './nav.css'
 
 export default function Nav () {
     return(
-        <div className='nav'>
-            <ul>
-                <li>
-                    <Link to="/">Inicio</Link>
-                </li>
-                <li>
-                    <Link to="/que-hacemos">Quiénes somos</Link>
-                </li>
-                <li>
-                <   Link to="/como-ayudar">Cómo colaborar</Link>
-                </li>
-                <li>
-                    <Link to="/agenda">Agenda</Link>
-                </li>
-                <li>
-                    <Link to="/contacto">Contacto</Link>
-                </li>
-            </ul>
-        </div>
+        <Router>
+            <div className='nav'>
+                <ul>
+                    <li>
+                        <Link to="/">Inicio</Link>
+                    </li>
+                    <li>
+                        <Link to="/que-hacemos">Quiénes somos</Link>
+                    </li>
+                    <li>
+                    <   Link to="/como-ayudar">Cómo colaborar</Link>
+                    </li>
+                    <li>
+                        <Link to="/agenda">Agenda</Link>
+                    </li>
+                    <li>
+                        <Link to="/contacto">Contacto</Link>
+                    </li>
+                </ul>
+            </div>
+
+        </Router>
     )
 }
