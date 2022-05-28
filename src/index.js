4ccb5016001a6acfa5c2219d6edaee1c4232b98c
emilioMarchi <em.98.nmynm@gmail.com>
2022-05-28 14:05:01 -0300

    new commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 
@@ -10,11 +10,11 @@ import Footer from "./components/footer/Footer";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter>
+    <Router>
       <Header/>
         <App />
       <Footer/>
-    </BrowserRouter>
+    </Router>
   </React.StrictMode>
 );
 
