43c5f058cabd2d534124798317d38dc7cfd0b57a
emilioMarchi <em.98.nmynm@gmail.com>
2022-06-13 12:42:14 -0300

    nuevo commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,40 +1,19 @@
 import React from 'react' 
 
 import './footer.css'
-import socialBar from '../socialBar/socialBar'
+import SocialBar from '../socialBar/SocialBar'
 
 export default function Footer() {
     return(
         <div className='footer '>
-            <div className='footer-nav col'>
-                <ul>
-                    <li>
-                        <a href='/'>Inicio</a>
-                    </li>
-                    <li>
-                        <a href='/quienes-somos'>¿Quiénes somos?</a>
-                    </li>
-                    <li>
-                        <a href='/como-ayudar'>¿Cómo colaborar?</a>
-                    </li>
-                    <li>
-                        <a href='/agenda'>Agenda</a>
-                    </li>
-                    <li>
-                        <a href='/contacto'>Contacto</a>
-                    </li>
-                </ul>
-            </div>
-            <div>
-                <socialBar/>
-            </div>
+            <SocialBar/>
             <div className='info-contact col-6'>
-                <div className='info col'>
+                <div className='info'>
                     <h5>Email: email@gmail.com</h5>
                     <h5>Tel: 4-555-00-55</h5>
                     <h5>Direc: Calle 1154</h5>
                 </div>
-                <div className='map col'>map</div>
+                <div className='map'>map</div>
             </div>
         </div>
     )
--- src/components/socialBar/socialBar.js
+++ src/components/socialBar/socialBar.js
@@ -1,18 +1,21 @@
 import React from 'react'
 
+import './socialBar.css'
 
-
-export default function socialBar() {
+export default function SocialBar() {
     return(
-        <div>
+        <div className='social-bar'>
+            
             <ul>
                 <li>
-                    <a>
-                        <img src='./img/icons/fb-white.png' />
+                    <a href='https://www.facebook.com' target='_blank'>
+                        <img className='icon' src='./img/icons/fb-white.png'/>
                     </a>
                 </li>
                 <li>
-                    <a></a>
+                    <a href='https://www.instagram.com' target='_blank'>
+                        <img className='icon' src='./img/icons/instagram.png'/>
+                    </a>
                 </li>
             </ul>
         </div>
