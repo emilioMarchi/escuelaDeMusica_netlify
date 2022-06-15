971931d14e8c71b2800eb9f4ce4323b69bed4658
emilioMarchi <em.98.nmynm@gmail.com>
2022-06-15 15:46:01 -0300

    nuevo commit

--- src/components/agend/Agend.js
+++ src/components/agend/Agend.js
@@ -1,8 +1,5 @@
 import React from 'react' 
 
-import AgendCarousel from './agendCarousel/AgendCarousel'
-import NewCarousel from './newCarousel/NewCarousel'
-
 export default function Agend () {
     return(
         <>
@@ -11,8 +8,27 @@ export default function Agend () {
                             <h2>Agenda</h2>
                         </div>
                         <div className='agenda-items'>
-                            <AgendCarousel/>
-                            <NewCarousel/>
+                            <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <h2>algo</h2>
+                                    </div>
+                                    <div class="carousel-item">
+                                        <h2>algo</h2>                                    
+                                    </div>
+                                    <div class="carousel-item">
+                                        <h2>algo</h2>                                    
+                                    </div>
+                                </div>
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                                </div>
 
                         </div>
                     </div>
