e747c4f6fcb54fab195e36f28de0e0adf28eb55f
emilioMarchi <em.98.nmynm@gmail.com>
2022-06-15 14:25:20 -0300

    nuevo commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -2,14 +2,9 @@ import React, {useMemo, useEffect} from 'react'
 import { GoogleMap, useLoadScript, Marker } from '@react-google-maps/api';
 
 import './footer.css'
-import SocialBar from './socialBar/SocialBar'
-
+import './socialBar.css'
 
 export default function Footer() {
-    
-    const {isLoaded} = useLoadScript({
-        googleMapsApiKey:"AIzaSyDgBV7FsYzoXGjYVOzg_zAG4YIS19KOE0I"
-    })
 
     const Map = () => {
         const center = useMemo(() => ({lat:-31.6308448, lng:-60.7228647}), [])
@@ -25,8 +20,22 @@ export default function Footer() {
     }
     return(
         <div className='footer'>
-            <SocialBar/>
-            <h2>socialbar</h2>
+            <div className='redes-bar'>
+        
+                <ul>
+                    <li>
+                        <a href='https://www.facebook.com' target='_blank'>
+                            <img className='icon' src='./img/icons/fb-white.png'/>
+                        </a>
+                    </li>
+                    <li>
+                        <a href='https://www.instagram.com' target='_blank'>
+                            <img className='icon' src='./img/icons/instagram.png'/>
+                        </a>
+                    </li>
+                </ul>
+            </div>
+            
             <div className='info-contact col-6'>
                 <div className='info'>
                     <h5>Email: email@gmail.com</h5>
