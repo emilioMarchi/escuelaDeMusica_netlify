de1cb865ee4784a80c2b16317f8dd6f86bde34e2
emilioMarchi <em.98.nmynm@gmail.com>
2022-06-15 16:31:01 -0300

    nuevo commit

--- src/components/agend/Agend.js
+++ src/components/agend/Agend.js
@@ -1,23 +1,34 @@
 import React from 'react' 
+import './Agend.css'
 
 export default function Agend () {
+
+
+    const AgendItem = () => {
+        return(
+            <div className='agend-item'>
+                <h2>Title</h2>
+                <h5>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto.</h5>
+            </div>
+        )
+    }
     return(
         <>
-            <div className='agenda-slider col-4'>
-                        <div className='agenda-title'>
-                            <h2>Agenda</h2>
+            <div className='agend-slider col-4'>
+                        <div className='agend-title'>
+                            <h2>Agenda / Novedades</h2>
                         </div>
-                        <div className='agenda-items'>
+                        <div className='agend-items'>
                             <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
                                 <div class="carousel-inner">
                                     <div class="carousel-item active">
-                                        <h2>algo</h2>
+                                        <AgendItem />
                                     </div>
                                     <div class="carousel-item">
-                                        <h2>algo</h2>                                    
+                                        <AgendItem />                                    
                                     </div>
                                     <div class="carousel-item">
-                                        <h2>algo</h2>                                    
+                                        <AgendItem />                                    
                                     </div>
                                 </div>
                                 <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
@@ -28,8 +39,28 @@ export default function Agend () {
                                     <span class="carousel-control-next-icon" aria-hidden="true"></span>
                                     <span class="visually-hidden">Next</span>
                                 </button>
+                            </div>
+                            <div id="carouselExampleControls2" class="carousel slide" data-bs-ride="carousel">
+                                <div class="carousel-inner">
+                                    <div class="carousel-item active">
+                                        <AgendItem />
+                                    </div>
+                                    <div class="carousel-item">
+                                        <AgendItem />                                    
+                                    </div>
+                                    <div class="carousel-item">
+                                        <AgendItem />                                    
+                                    </div>
                                 </div>
-
+                                <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="prev">
+                                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Previous</span>
+                                </button>
+                                <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControls2" data-bs-slide="next">
+                                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                                    <span class="visually-hidden">Next</span>
+                                </button>
+                            </div>
                         </div>
                     </div>
         </>
