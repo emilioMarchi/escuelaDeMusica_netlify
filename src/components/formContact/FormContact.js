c11dc7b426379b04aa297222de6a64e5a443e019
emilioMarchi <em.98.nmynm@gmail.com>
2022-10-30 07:54:58 -0300

    commit

--- src/components/formContact/FormContact.js
+++ src/components/formContact/FormContact.js
@@ -38,7 +38,7 @@ const FormContact = () => {
           
           await axios({
             method: 'post',
-            url:`https://api.escuelademusicabarrial.ar/api/contact`,
+            url:`https://api.escuelademusicabarrial.ar/contact`,
             withCredentials: false,
             data: values
           }).then((res)=>{
