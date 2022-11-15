0bbd1d3cefa004f3fda8816a8a043d56e8fdaf6d
emilioMarchi <em.98.nmynm@gmail.com>
2022-11-15 15:27:29 -0300

    commit

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,4 +1,6 @@
-import React, {useMemo, useEffect} from 'react' 
+import React from 'react' 
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faLocationDot } from '@fortawesome/free-solid-svg-icons'
 
 import './footer.css'
 import './socialBar.css'
@@ -13,8 +15,12 @@ export default function Footer() {
                 
                 
                 <div className='info-contact'>
-                    <div className='info'>
-                        
+                    <div className='info' >
+                        <a className='location-link' target='_blank' href={'https://www.google.com/maps/place/Escuela+de+M%C3%BAsica+Barrial/@-31.6308086,-60.7206793,21z/data=!4m5!3m4!1s0x95b5a9cd289e61e5:0x9be604c0a1c2de9a!8m2!3d-31.6308466!4d-60.7206772?hl=es-ES'}>
+                            <FontAwesomeIcon  className='location-icon' icon={faLocationDot} />
+
+                            <p>Ver en mapa</p>
+                        </a>
                         <h5>Padre Catena 4200 - Santa Fe - 3000 - Santa Fe</h5>
                         <h5>escuelademusicabarrialsf@gmail.com</h5>
                       
--- src/components/map/Map.js
+++ src/components/map/Map.js
@@ -2,7 +2,7 @@ import React from 'react'
 import GoogleMap from 'simple-react-google-maps'
 
 export default function Map() {
-    const center = {lat:-31.6308448, lng:-60.7228647}
+    const center = {lat:-31.6308286, lng:-60.7206793}
 
     const Marker = () => {
         return(
@@ -21,11 +21,11 @@ export default function Map() {
             
             apiKey={'AIzaSyDgBV7FsYzoXGjYVOzg_zAG4YIS19KOE0I'}
             style={{height:'100%', width: '100%'}}
-            zoom={15}
+            zoom={18}
             center={center}
-            markers={[{lat:-31.6308286, lng:-60.7206793, title:'nombre'}]}
+            markers={[{lat:-31.6308286, lng:-60.7206793, namespace:'nombre'}]}
             mapContainerClassName='map-container'
-            src='https://goo.gl/maps/n7R3n76aqGM2Dvzf8'
+            
             >
         </GoogleMap>
 
